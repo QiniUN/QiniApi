42374a36c0e4066d6e0d70737ded3cb1ab9bbca6
Dorian Abad Tovar Díaz <abadtovar@gmail.com>
2017-11-22 02:54:22 -0500

    Added request - response HTTP

--- app/controllers/station_controller.rb
+++ app/controllers/station_controller.rb
@@ -1,4 +1,23 @@
 class StationController < ApplicationController
+  def getStation
+    @idEstacion = Integer(params["idEstacion"])
+    @horario = params["horario"]
 
-  
+    @data = { esperaActual: 5, esperaPromedio: 7, fila: 5, ciclas: 10 }
+    render json: @data
+  end
+
+  def postStation
+    @id = Integer( params[:id] )
+    @tiempo = Float(params[:tiempo])
+    @fila = Integer(params[:fila])
+    @servidores = Integer(params[:servidores])
+
+    render json: { id: @id, tiempo: @tiempo, fila: @fila, servidores: @servidores }
+  end
+
+  private
+  def postParams
+    params.require(  ).permit( :id, :tiempo, :fila, :servidores )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "station/getStation/:idEstacion/:horario" => "station#getStation"
+
+  post "station/postStation" => "station#postStation"
 end
